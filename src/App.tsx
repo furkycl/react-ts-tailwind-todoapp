df8a027ce064fe526b2adca4e4f8ebebdb525fc1
Furkan YÜCEL <81173127+furkycl@users.noreply.github.com>
2025-07-30 22:42:09 +0300

    chore: Project cleaned from default vite settings.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 function App() {
   return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-900 text-white">
-      <h1 className="text-4xl font-bold text-sky-500">
-        ToDo List - React, TypeScript & Tailwind CSS
-      </h1>
+    <div className="bg-gray-900 min-h-screen text-white p-4">
+      <h1 className="text-center text-3xl font-bold">ToDo List</h1>
     </div>
   );
 }
