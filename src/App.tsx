1f9ba6c51c46367b902e5436470424041aad8adf
Furkan YÜCEL <81173127+furkycl@users.noreply.github.com>
2025-07-31 00:59:54 +0300

    feat(state): Initialize todo list state with with dummy data

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
+import { useState } from "react";
+import type { Todo } from "./types";
+
+const initialTodos: Todo[] = [
+  {
+    id: "01",
+    text: "React Projesini Tamamla",
+    completed: false,
+  },
+  {
+    id: "02",
+    text: "TypeScript Öğren.",
+    completed: true,
+  },
+  {
+    id: "03",
+    text: "Tailwind ile CSS ver.",
+    completed: false,
+  },
+];
+
 function App() {
+  const [todos, setTodos] = useState<Todo[]>(initialTodos);
   return (
     <div className="bg-gray-900 min-h-screen text-white p-4">
-      <h1 className="text-center text-3xl font-bold">ToDo List</h1>
+      <h1 className="text-center text-3xl font-bold mb-8">ToDo List</h1>
     </div>
   );
 }
